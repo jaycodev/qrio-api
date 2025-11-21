7461a417fb2575aeaeaab6d85b3d7e6d177220f3
jaycodev <jason.vilac@gmail.com>
2025-11-21 14:42:33 -0500

    Initial commit

---
+++ src/test/java/com/qrio/QrioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qrio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
